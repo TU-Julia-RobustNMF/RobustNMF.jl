2c2eaaa33359d012da6d98436f5ae88c63213c2f
Adrian Brag <135387710+cubi4@users.noreply.github.com>
2026-01-18 15:03:01 +0100

    Implement Robust NMF and streamline project structure (#9)
    * implemented robustNMF
    
    * adjusted project.toml according to the feedbacks
    
    * removed (flattened) submodules and included funcitons as files
    
    * removed unused doc md

--- src/StandardNMF.jl
+++ src/StandardNMF.jl
@@ -1,8 +1,4 @@
-module StandardNMF
-
 using LinearAlgebra, Random
-
-export nmf, X_reconstruct
 
 """
      nmf(X; rank::Int=10, maxiter::Int=500, tol::Float64=1e-4)
@@ -22,8 +18,7 @@ function nmf(X; rank::Int = 10, maxiter::Int = 500, tol::Float64 = 1e-4)
         m, n = size(X)
         W = rand(m, rank)
         H = rand(rank, n)
-        
-        @assert minimum(X) ≥ 0 "X must be non-negative"
+        @assert minimum(X) >= 0 "X must be non-negative"
 
         history = zeros(maxiter)
         prev_err = Inf
@@ -46,4 +41,3 @@ function nmf(X; rank::Int = 10, maxiter::Int = 500, tol::Float64 = 1e-4)
         return W, H, history
 end
 
-end # end module 
