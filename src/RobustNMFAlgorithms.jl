652e1d7366132789bf2ce9e9f6efa1ccadbc52c8
Yernur123 <110782135+Yernur123@users.noreply.github.com>
2026-01-26 16:39:39 +0100

    Update RobustNMFAlgorithms.jl

--- src/RobustNMFAlgorithms.jl
+++ src/RobustNMFAlgorithms.jl
@@ -119,7 +119,6 @@ X = rand(50, 30)
 F, G, hist = robustnmf(X; rank=5, maxiter=200)
 ```
 """
-#RobustNMF function
 function robustnmf(X::AbstractMatrix{<:Real}; 
                  rank::Int=10, 
                  maxiter::Int=500, 
